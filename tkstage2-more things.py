aaa17a945f7195c241076b750f66ee32c275e1ed
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2024-09-20 14:05:54 +0100

    Update tkstage2-more things.py
    Canvas things

--- tkstage2-more things.py
+++ tkstage2-more things.py
@@ -1,5 +1,6 @@
 #Stage 2 - more things
 import tkinter as tk
+import random
 
 #Your application must inherit from Frame
 class Application(tk.Frame):
@@ -12,10 +13,13 @@ class Application(tk.Frame):
     def createWidgets(self):
         #Add stuff to my application
         #bg is background colour
+        #canvas bd is border width for canvas
         self.testButton = tk.Button(self,text="Test",bg="#00ff00",command=self.myTest)
         self.quitButton = tk.Button(self,text="Quit",command=self.myQuit)
+        self.canvas1 = tk.Canvas(self,height=100,width=60,bd=10,relief=tk.RIDGE)
         self.testButton.grid(column = 0,row=0,padx=20,pady=20) #places the button on the app frame
         self.quitButton.grid(column = 1,row=0,padx=20,pady=20)
+        self.canvas1.grid(row=2)
     
     def myTest(self):
         if(self.cursorType == "arrow"):
@@ -24,6 +28,12 @@ class Application(tk.Frame):
             self.cursorType = "arrow"
             
         self.config(cursor=self.cursorType)
+        
+        #Draw on the canvas1
+        self.canvas1.delete("all")
+        randx=random.randint(10,20)
+        randy=random.randint(10,20)
+        self.canvas1.create_rectangle(1,1,20+randx,30+randy)
         
     def myQuit(self):
         quit()
