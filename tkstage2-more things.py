ecb65e0a71ff528ea00f6b7a36cf1ff7b3e351ba
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2024-09-22 17:25:07 +0100

    Update tkstage2-more things.py

--- tkstage2-more things.py
+++ tkstage2-more things.py
@@ -2,13 +2,15 @@
 import tkinter as tk
 import random
 
+CANVAS_BORDER = 5
+
 #Your application must inherit from Frame
 class Application(tk.Frame):
     def __init__(self,master=None):
         tk.Frame.__init__(self,master)  #call parent constructor
         self.grid()  #necessary to make it appear on the screen!
         self.createWidgets()
-        self.config(cursor="circle")
+        self.config(cursor="hand2")
         
     def createWidgets(self):
         #Add stuff to my application
@@ -16,16 +18,16 @@ class Application(tk.Frame):
         #canvas bd is border width for canvas
         self.testButton = tk.Button(self,text="Test",bg="#00ff00",command=self.myTest)
         self.quitButton = tk.Button(self,text="Quit",command=self.myQuit)
-        self.canvas1 = tk.Canvas(self,height=100,width=60,bd=10,relief=tk.RIDGE)
+        self.canvas1 = tk.Canvas(self,height=100,width=60,bd=CANVAS_BORDER,relief=tk.RIDGE)
         self.testButton.grid(column = 0,row=0,padx=20,pady=20) #places the button on the app frame
         self.quitButton.grid(column = 1,row=0,padx=20,pady=20)
         self.canvas1.grid(row=2)
     
     def myTest(self):
-        if(self.cursorType == "arrow"):
-            self.cursorType = "watch"
+        if(self.cursorType == "hand2"):
+            self.cursorType = "umbrella"
         else:
-            self.cursorType = "arrow"
+            self.cursorType = "hand2"
             
         self.config(cursor=self.cursorType)
         
@@ -33,12 +35,33 @@ class Application(tk.Frame):
         self.canvas1.delete("all")
         randx=random.randint(10,20)
         randy=random.randint(10,20)
-        self.canvas1.create_rectangle(1,1,20+randx,30+randy)
+        theID = self.canvas1.create_rectangle(CANVAS_BORDER+3,CANVAS_BORDER+3,20+randx,30+randy)
+        randx=random.randint(10,20)
+        randy=random.randint(10,20)
+        theID = self.canvas1.create_arc(CANVAS_BORDER+3,CANVAS_BORDER+3,20+randx,30+randy,style=tk.ARC)
+        randx=random.randint(10,20)
+        randy=random.randint(10,20)
+        theID = self.canvas1.create_bitmap(CANVAS_BORDER+3+randx,CANVAS_BORDER+3+randy,bitmap="warning")
+
+         
+        print(theID)
+        somecoords = self.canvas1.coords(theID)
+        print(somecoords)
+
+        somecoords = self.canvas1.bbox(theID)
+        print(somecoords)
+
+        someResult = self.canvas1.find_all()
+        print(someResult)
+
+        someResult = self.canvas1.type(theID)
+        print(someResult)
+        
         
     def myQuit(self):
         quit()
         
-    cursorType = "arrow"
+    cursorType = "hand2"
         
 app = Application()
 app.master.title("TK Helloworld")
